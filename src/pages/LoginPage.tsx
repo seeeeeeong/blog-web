538d124b04c89a994482975cbbf61040a6a733c6
“seeeeeeong” <lsinsung@gmail.comgit config --global user.email lsinsung@gmail.com>
2025-12-14 01:15:50 +0900

    fix: 오류 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
   };
 
   return (
-    <AuthLayout title="Admin Login">
+    <AuthLayout title="Admin Login" subtitle="관리자 계정으로 로그인하세요">
       <form onSubmit={handleSubmit} className="space-y-6">
         <div>
           <label
